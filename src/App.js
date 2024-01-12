cf0f434a129fbae8c06c1800431acd1e1b58c0c8
BERCHNARD10 <20210643@uthh.edu.mx>
2024-01-12 10:29:07 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import './assets/styles/responsive.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import HomePage from './pages/HomePage';  
+import PrivacyPolicy from './pages/Terminos';
+import Cockies from './pages/Cockies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout><HomePage /></Layout>}
+        />
+        <Route
+          path="/Terminos-Condiciones"
+          element={<Layout><PrivacyPolicy/></Layout>}
+        />
+        <Route
+          path="/Politica-de-Cockies"
+          element={<Layout><Cockies/></Layout>}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
